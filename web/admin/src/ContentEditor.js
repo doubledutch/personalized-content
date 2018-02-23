99dac5480b00672ea9ae2e63fd886c634fec0c39
Adam Liechty <adam@doubledutch.me>
2018-02-23 11:19:20 -0800

    factor ContentDetailsEditor into new component

--- web/admin/src/ContentEditor.js
+++ web/admin/src/ContentEditor.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
-import { TextEditor } from './editors'
 import AttendeeSelector from './AttendeeSelector'
+import ContentDetailsEditor from './ContentDetailsEditor'
 
 export default class ContentEditor extends PureComponent {
   constructor(props) {
@@ -17,28 +17,8 @@ export default class ContentEditor extends PureComponent {
         </div>
         <div>TODO - Content editor goes here for content w/ key: "{key}"</div>
         <AttendeeSelector content={content} onUpdate={onUpdate} allUsers={this.props.allUsers} />
-        { this.editorFor(content) }
+        <ContentDetailsEditor content={content} onUpdate={onUpdate} />
       </div>
     )
-  }
-
-  editorFor = c => {
-    const {onUpdate} = this.props
-    switch (c.type) {
-      case 'text': return <div>
-        <TextEditor content={c} prop="title" title="Title" onUpdate={onUpdate} />
-        <TextEditor content={c} prop="text" title="Text" onUpdate={onUpdate} />
-        <div>{JSON.stringify(c)}</div>
-      </div>
-      case 'web': return <div>
-        <TextEditor content={c} prop="title" title="Title" onUpdate={onUpdate} />
-        <TextEditor content={c} prop="url" title="URL" onUpdate={onUpdate} />
-        <div>{JSON.stringify(c)}</div>
-      </div>
-      case 'survey': return <div>
-        <div>{JSON.stringify(c)}</div>
-      </div>
-      default: return <div />
-    }
   }
 }
