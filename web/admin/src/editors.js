60a24c7b2a683cff0e1d33a726fe48e1d05e4f6d
Eddie Stamps <estamps@doubledutch.me>
2018-03-20 20:05:17 +0000

    QAS Merge Comment Fixes

--- web/admin/src/editors.js
+++ web/admin/src/editors.js
@@ -56,8 +56,9 @@ export class TextEditor extends PureComponent {
 
   isTitle = () => {
     if (this.props.isTitle) {
+      var total = 150 - 0
       return (
-        <p className="text-editor__counter">{150 - (this.state.value) ? this.state.value : 0} </p>
+        <p className="text-editor__counter">{150 - (this.state.value ? this.state.value.length : 0)} </p>
       )
     }
   }
