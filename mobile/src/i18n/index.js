1c70d5739ddc84c93ab393514b289af21a7acd91
Adam Liechty <adam@doubledutch.me>
2018-12-03 13:48:51 -0800

    fix i18n file naming

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,5 +1,5 @@
-import enUS from './personalized-content.mobile.en-US.json'
-// import deDE from './personalized-content.mobile.de-DE.json'
+import enUS from './en-us/personalized-content.mobile.json'
+// import deDE from './de-de/personalized-content.mobile.json'
 
 export default (strings = {
   '': enUS,
